bf4d29fe5072513a1ffb6866b9c6deb0875172cd
Benedikt Terhechte <terhechte@gmail.com>
2015-07-05 19:50:35 +0200

    Simplified and working way of converting structures into core data objects via reflection.

--- StructData/StructData.swift
+++ StructData/StructData.swift
@@ -7,3 +7,47 @@
 //
 
 import Foundation
+import CoreData
+public protocol NSManagedStruct {
+    var EntityName: String {get}
+}
+
+public enum NSManagedStructError : ErrorType {
+    case StructConversionError()
+}
+public func toCoreData(context: NSManagedObjectContext)(entity: NSManagedStruct) throws -> NSManagedObject {
+    
+    let mirror = Mirror(reflecting: entity)
+    
+    if let style = mirror.displayStyle where style == .Struct || style == .Class {
+        
+        // try to create an entity
+        let desc = NSEntityDescription.entityForName(entity.EntityName, inManagedObjectContext:context)
+        let result = NSManagedObject(entity: desc!, insertIntoManagedObjectContext: nil)
+        
+        for (labelMaybe, valueMaybe) in mirror.children {
+            
+            guard let label = labelMaybe else {
+                continue
+            }
+            
+            if ["EntityName"].contains(label) {
+                continue
+            }
+            
+            switch valueMaybe {
+            case let k as Int16:
+                result.setValue(NSNumber(short: k), forKey: label)
+            case let k as AnyObject:
+                result.setValue(k, forKey: label)
+            default:
+                continue
+            }
+            
+        }
+        
+        return result
+    }
+    
+    throw NSManagedStructError.StructConversionError()
+}
