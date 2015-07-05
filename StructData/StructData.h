c62535072616b3d49ae9fc7cd18477f27a49da02
Benedikt Terhechte <terhechte@gmail.com>
2015-07-05 17:51:25 +0200

    Initial Commit

---
+++ StructData/StructData.h
@@ -0,0 +1,19 @@
+//
+//  StructData.h
+//  StructData
+//
+//  Created by Benedikt Terhechte on 05/07/15.
+//  Copyright © 2015 Benedikt Terhechte. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StructData.
+FOUNDATION_EXPORT double StructDataVersionNumber;
+
+//! Project version string for StructData.
+FOUNDATION_EXPORT const unsigned char StructDataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StructData/PublicHeader.h>
+
+
