d13eeb9ece2e021d23de31de372eed41e02b6dba
Benedikt Terhechte <terhechte@gmail.com>
2015-07-05 19:48:19 +0200

    Added temporary Mac target (should later change this to using a workspace) for easier local testing & development

---
+++ StructDataMac/StructDataMac.h
@@ -0,0 +1,19 @@
+//
+//  StructDataMac.h
+//  StructDataMac
+//
+//  Created by Benedikt Terhechte on 05/07/15.
+//  Copyright © 2015 Benedikt Terhechte. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for StructDataMac.
+FOUNDATION_EXPORT double StructDataMacVersionNumber;
+
+//! Project version string for StructDataMac.
+FOUNDATION_EXPORT const unsigned char StructDataMacVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StructDataMac/PublicHeader.h>
+
+
