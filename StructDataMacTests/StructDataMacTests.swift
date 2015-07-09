c33f58110be7aebb63acad71839e76c65d21b5c7
Benedikt Terhechte <terhechte@gmail.com>
2015-07-09 13:24:59 +0200

    Added tests for the current working state.

--- StructDataMacTests/StructDataMacTests.swift
+++ StructDataMacTests/StructDataMacTests.swift
@@ -8,15 +8,26 @@
 
 import XCTest
 
+
 struct Employee : NSManagedStruct {
     
     let EntityName = "Employee"
     
     var name: String
     var age: Int16
-    var position: String
+    var position: String?
     var department: String
     var job: String
+    
+    // FIXME: Relationship support
+    static func fromObject(o: NSManagedObject) -> Unboxed<Employee> {
+        let x = curry(self.init) <^> o <| "name"
+        <*> o <| "age"
+        <*> o <|? "position"
+        <*> o <| "department"
+        <*> o <| "job"
+        return x
+    }
 }
 
 func setUpInMemoryManagedObjectContext(cls: AnyClass) -> NSManagedObjectContext? {
@@ -41,13 +52,27 @@ func setUpInMemoryManagedObjectContext(cls: AnyClass) -> NSManagedObjectContext?
 
 class StructDataMacTests: XCTestCase {
     
-    var context: NSManagedObjectContext?
+    var context: NSManagedObjectContext = {
+        return setUpInMemoryManagedObjectContext(StructDataMacTests)!
+    }()
+    
+    let employee1 = {
+        return Employee(name: "John Doe", age: 20, position: "Manager", department: "Flowers", job: "Garden Guy")
+    }()
+    
+    let employee2 = {
+        return Employee(name: "Nobody", age: 14, position: nil, department: "Kindergarten", job: "Playing")
+    }()
+    
+    var nsEmployee1: NSManagedObject!
+    
+    var nsEmployee2: NSManagedObject!
+    
     
     override func setUp() {
         super.setUp()
-        if let c = setUpInMemoryManagedObjectContext(StructDataMacTests) {
-            self.context = c
-        }
+        self.nsEmployee1 = try! toCoreData(self.context)(entity: self.employee1)
+        self.nsEmployee2 = try! toCoreData(self.context)(entity: self.employee2)
     }
     
     override func tearDown() {
@@ -55,22 +80,69 @@ class StructDataMacTests: XCTestCase {
         super.tearDown()
     }
     
-    func testToCoreData() {
-        if let ctx = self.context {
-            let emp = Employee(name: "klaus", age: 20, position: "Manager", department: "Blumen", job: "Lustiger")
-            do {
-                let cd = try toCoreData(ctx)(entity: emp)
-                if (cd.valueForKey("name") as! String) != emp.name {
-                    XCTAssert(false, "Conversion failed: name")
-                }
-                if (cd.valueForKey("age") as! NSNumber).integerValue != Int(emp.age) {
-                    XCTAssert(false, "Conversion failed: age")
-                }
-            } catch _ {
-            XCTAssert(false, "Conversion failed")
+    func testToCoreDataNonNil() {
+        do {
+            let cd = try toCoreData(self.context)(entity: self.employee1)
+            if (cd.valueForKey("name") as! String) != self.employee1.name {
+                XCTAssert(false, "Conversion failed: name")
             }
-        } else {
-            XCTAssert(false, "Could not create context")
+            if (cd.valueForKey("age") as! NSNumber).integerValue != Int(self.employee1.age) {
+                XCTAssert(false, "Conversion failed: age")
+            }
+        } catch NSManagedStructError.StructConversionError(let msg) {
+            XCTAssert(false, msg)
+        } catch NSManagedStructError.StructValueError(let msg) {
+            XCTAssert(false, msg)
+        } catch let e {
+            print(e)
+            XCTAssert(false, "An Error Occured")
+        }
+    }
+    
+    func testToCoreDataNil() {
+        do {
+            let cd = try toCoreData(self.context)(entity: self.employee2)
+            if (cd.valueForKey("name") as! String) != self.employee2.name {
+                XCTAssert(false, "Conversion failed: name")
+            }
+            if (cd.valueForKey("age") as! NSNumber).integerValue != Int(self.employee2.age) {
+                XCTAssert(false, "Conversion failed: age")
+            }
+            if (cd.valueForKey("position") != nil) {
+                XCTAssert(false, "Conversion failed: age")
+            }
+        } catch NSManagedStructError.StructConversionError(let msg) {
+            XCTAssert(false, msg)
+        } catch NSManagedStructError.StructValueError(let msg) {
+            XCTAssert(false, msg)
+        } catch let e {
+            print(e)
+            XCTAssert(false, "An Error Occured")
+        }
+    }
+    
+    func testFromCoreDataNonNil() {
+        switch Employee.fromObject(self.nsEmployee1) {
+        case .Success(let t):
+            if t.name != self.employee1.name ||
+               t.age != self.employee1.age {
+                XCTAssert(false, "Conversion Error")
+            }
+        case .TypeMismatch(let msg):
+            XCTAssert(false, msg)
+        }
+    }
+    
+    func testFromCoreDataNil() {
+        switch Employee.fromObject(self.nsEmployee2) {
+        case .Success(let t):
+            if t.name != self.employee2.name ||
+               t.age != self.employee2.age ||
+               t.position != nil {
+                XCTAssert(false, "Conversion Error")
+            }
+        case .TypeMismatch(let msg):
+            XCTAssert(false, msg)
         }
     }
     
